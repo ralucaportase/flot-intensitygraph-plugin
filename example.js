f2860bf19916448e4b40989e2f137a3ce025e1f0
Ciprian Ceteras <cipix2000@gmail.com>
2015-11-04 22:47:57 +0200

    grid above map and added license for the music

--- example.js
+++ example.js
@@ -2,58 +2,73 @@
 /*jshint browser: true*/
 
 $(function () {
-    'use strict';
-    var plot;
-    var offset = 0.0;
-    var h  = 51;
-    var w = 101;
-    var iMap = [];
+  'use strict';
+  var plot;
+  var offset = 0.0;
+  var h = 51;
+  var w = 101;
+  var iMap = [];
 
-    function updateData() {
-        iMap = [];
-        for (var i = 0; i < w; i ++) {
-            for (var j = 0; j < h; j ++) {
-                iMap.push([i, j, i+j]);
-            }
-        }
+  function updateData() {
+    iMap = [];
+    for (var i = 0; i < w; i++) {
+      for (var j = 0; j < h; j++) {
+        iMap.push([i, j, i + j]);
+      }
     }
+  }
 
-    function updateGraph() {
-        setTimeout(updateGraph, 16);
+  function updateGraph() {
+    setTimeout(updateGraph, 16);
 
-        if ($('#checkbox').prop('checked')) {
-            updateData();
+    if ($('#checkbox').prop('checked')) {
+      updateData();
 
-            plot.setData([
-                {
-                    data: iMap
-                }
-            ]);
+      plot.setData([{
+        data: iMap
+      }]);
 
-            plot.setupGrid();
-            plot.draw();
-        }
+      plot.setupGrid();
+      plot.draw();
     }
+  }
 
-    updateData();
-    plot = $.plot("#placeholder", [
-        {
-            data: iMap
+  updateData();
+  plot = $.plot("#placeholder", [{
+    data: iMap
+  }], {
+    series: {
+      intensitymap: {
+        active: true,
+        show: true,
+        max: 160,
+        radius: 3.5,
+        gradient: {
+          0: 'red',
+          0.12: 'orange',
+          0.25: 'yellow',
+          0.37: 'lightgreen',
+          0.5: 'cyan',
+          0.62: 'lightblue',
+          0.75: 'indigo',
+          0.9: 'violet',
+          1: 'white'
         }
-    ], {
-        series: {
-            intensitymap: {
-                active: true,
-                show: true,
-                max: 160,
-                radius : 3.5,
-                gradient : { 0: 'red', 0.12: 'orange', 0.25: 'yellow', 0.37: 'lightgreen', 0.5: 'cyan', 0.62: 'lightblue', 0.75: 'indigo', 0.9: 'violet', 1: 'white'}
 
-            }
-        },
-        xaxis: { min: -10, max: 110 },
-        yaxis: { min: -10, max: 60 },
-    });
+      }
+    },
+    xaxis: {
+      min: -10,
+      max: 110
+    },
+    yaxis: {
+      min: -10,
+      max: 60
+    },
+    grid: {
+      aboveData: true
+    }
+  });
 
-    updateGraph();
+  updateGraph();
 });
